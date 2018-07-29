0d5785bc851abcc57196b718b763090d2da6ba7c
Juan Leni <jleni@users.noreply.github.com>
2018-07-29 12:43:26 +0200

    Initial implementation

---
+++ src/lib.rs
@@ -0,0 +1,304 @@
+/*******************************************************************************
+*   (c) 2020 ZondaX GmbH
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+********************************************************************************/
+//! Support library for Filecoin Ledger Nano S/X apps
+
+#![deny(warnings, trivial_casts, trivial_numeric_casts)]
+#![deny(unused_import_braces, unused_qualifications)]
+#![deny(missing_docs)]
+#![doc(html_root_url = "https://docs.rs/ledger-filecoin/0.1.0")]
+
+mod params;
+
+extern crate byteorder;
+extern crate ledger;
+#[macro_use]
+extern crate quick_error;
+extern crate secp256k1;
+
+use self::ledger::{ApduAnswer, ApduCommand};
+use self::params::{APDUErrors, PayloadType};
+use crate::params::{
+    CLA, INS_GET_ADDR_SECP256K1, INS_GET_VERSION, INS_SIGN_SECP256K1, USER_MESSAGE_CHUNK_SIZE,
+};
+use std::str;
+
+/// hex string utilities
+pub mod utils;
+
+/// Public Key Length
+const PK_LEN: usize = 33;
+
+quick_error! {
+    /// Ledger App Error
+    #[derive(Debug)]
+    pub enum Error {
+        /// Invalid version error
+        InvalidVersion{
+            description("This version is not supported")
+        }
+        /// The message cannot be empty
+        InvalidEmptyMessage{
+            description("message cannot be empty")
+        }
+        /// The size fo the message to sign is invalid
+        InvalidMessageSize{
+            description("message size is invalid (too big)")
+        }
+        /// Public Key is invalid
+        InvalidPK{
+            description("received an invalid PK")
+        }
+        /// No signature has been returned
+        NoSignature {
+            description("received no signature back")
+        }
+        /// The signature is not valid
+        InvalidSignature {
+            description("received an invalid signature")
+        }
+        /// The derivation is invalid
+        InvalidDerivationPath {
+            description("invalid derivation path")
+        }
+        /// Device related errors
+        Ledger ( err: ledger::Error ) {
+            from()
+            description("ledger error")
+            display("Ledger error: {}", err)
+            cause(err)
+        }
+        /// Device related errors
+        Secp256k1 ( err: secp256k1::Error ) {
+            from()
+            description("Secp256k1 error")
+            display("Secp256k1 error: {}", err)
+            cause(err)
+        }
+    }
+}
+
+/// Filecoin App
+pub struct FilecoinApp {
+    app: ledger::LedgerApp,
+}
+
+unsafe impl Send for FilecoinApp {}
+
+/// FilecoinApp address (includes pubkey and the corresponding ss58 address)
+pub struct Address {
+    /// Public Key
+    pub public_key: secp256k1::PublicKey,
+    /// Address
+    pub formatted: String,
+}
+
+/// FilecoinApp App Version
+pub struct Version {
+    /// Application Mode
+    pub mode: u8,
+    /// Version Major
+    pub major: u8,
+    /// Version Minor
+    pub minor: u8,
+    /// Version Patch
+    pub patch: u8,
+}
+
+/// BIP44 Path
+pub struct BIP44Path {
+    /// Purpose
+    pub purpose: u32,
+    /// Coin
+    pub coin: u32,
+    /// Account
+    pub account: u32,
+    /// Change
+    pub change: u32,
+    /// Address Index
+    pub index: u32,
+}
+
+fn serialize_bip44(path: &BIP44Path) -> Vec<u8> {
+    use byteorder::{LittleEndian, WriteBytesExt};
+    let mut m = Vec::new();
+    let harden = 0x8000_0000;
+    m.write_u32::<LittleEndian>(harden | path.purpose).unwrap();
+    m.write_u32::<LittleEndian>(harden | path.coin).unwrap();
+    m.write_u32::<LittleEndian>(path.account).unwrap();
+    m.write_u32::<LittleEndian>(path.change).unwrap();
+    m.write_u32::<LittleEndian>(path.index).unwrap();
+    m
+}
+
+impl FilecoinApp {
+    /// Connect to the Ledger App
+    pub fn connect() -> Result<Self, Error> {
+        let app = ledger::LedgerApp::new()?;
+        Ok(FilecoinApp { app })
+    }
+
+    /// Retrieve the app version
+    pub fn version(&self) -> Result<Version, Error> {
+        let command = ApduCommand {
+            cla: CLA,
+            ins: INS_GET_VERSION,
+            p1: 0x00,
+            p2: 0x00,
+            length: 0,
+            data: Vec::new(),
+        };
+
+        let response = self.app.exchange(command)?;
+        if response.retcode != APDUErrors::NoError as u16 {
+            return Err(Error::InvalidVersion);
+        }
+
+        if response.data.len() < 4 {
+            return Err(Error::InvalidVersion);
+        }
+
+        let version = Version {
+            mode: response.data[0],
+            major: response.data[1],
+            minor: response.data[2],
+            patch: response.data[3],
+        };
+
+        Ok(version)
+    }
+
+    /// Retrieves the public key and address
+    pub fn address(&self, path: &BIP44Path, require_confirmation: bool) -> Result<Address, Error> {
+        let serialized_path = serialize_bip44(path);
+        let p1 = if require_confirmation { 1 } else { 0 };
+
+        let command = ApduCommand {
+            cla: CLA,
+            ins: INS_GET_ADDR_SECP256K1,
+            p1,
+            p2: 0x00,
+            length: 0,
+            data: serialized_path,
+        };
+
+        match self.app.exchange(command) {
+            Ok(response) => {
+                if response.retcode != APDUErrors::NoError as u16 {
+                    println!("WARNING: retcode={:X?}", response.retcode);
+                }
+
+                if response.data.len() < PK_LEN {
+                    return Err(Error::InvalidPK);
+                }
+
+                let public_key = secp256k1::PublicKey::from_slice(&response.data[..PK_LEN])?;
+                let formatted = str::from_utf8(&response.data[33..]).unwrap().to_owned();
+
+                let address = Address {
+                    public_key,
+                    formatted,
+                };
+                Ok(address)
+            }
+            Err(err) => Err(Error::Ledger(err)),
+        }
+    }
+
+    /// Sign a transaction
+    pub fn sign(&self, path: &BIP44Path, message: &[u8]) -> Result<secp256k1::Signature, Error> {
+        let bip44path = serialize_bip44(&path);
+        let chunks = message.chunks(USER_MESSAGE_CHUNK_SIZE);
+
+        if chunks.len() > 255 {
+            return Err(Error::InvalidMessageSize);
+        }
+
+        if chunks.len() == 0 {
+            return Err(Error::InvalidEmptyMessage);
+        }
+
+        let packet_count = chunks.len() as u8;
+        let mut response: ApduAnswer;
+
+        let _command = ApduCommand {
+            cla: CLA,
+            ins: INS_SIGN_SECP256K1,
+            p1: PayloadType::Init as u8,
+            p2: 0x00,
+            length: bip44path.len() as u8,
+            data: bip44path,
+        };
+
+        response = self.app.exchange(_command)?;
+
+        // Send message chunks
+        for (packet_idx, chunk) in chunks.enumerate() {
+            let mut p1 = PayloadType::Add as u8;
+            if packet_idx == (packet_count - 1) as usize {
+                p1 = PayloadType::Last as u8
+            }
+
+            let _command = ApduCommand {
+                cla: CLA,
+                ins: INS_SIGN_SECP256K1,
+                p1,
+                p2: 0,
+                length: chunk.len() as u8,
+                data: chunk.to_vec(),
+            };
+
+            response = self.app.exchange(_command)?;
+        }
+
+        if response.data.is_empty() && response.retcode == APDUErrors::NoError as u16 {
+            return Err(Error::NoSignature);
+        }
+
+        // Last response should contain the answer
+        if response.data.len() < 3 {
+            return Err(Error::InvalidSignature);
+        }
+
+        let sig_buffer_len = response.data.len();
+        let sig_slice = &response.data[..sig_buffer_len];
+        let sig = secp256k1::Signature::from_der(sig_slice)?;
+
+        Ok(sig)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::utils::to_hex_string;
+    use crate::{serialize_bip44, BIP44Path};
+
+    #[test]
+    fn bip44() {
+        let path = BIP44Path {
+            purpose: 0x2c,
+            coin: 1,
+            account: 0x1234,
+            change: 0,
+            index: 0x5678,
+        };
+        let serialized_path = serialize_bip44(&path);
+        assert_eq!(serialized_path.len(), 20);
+        assert_eq!(
+            to_hex_string(&serialized_path),
+            "2c00008001000080341200000000000078560000"
+        );
+    }
+}
---
+++ tests/integration_test.rs
@@ -0,0 +1,180 @@
+/*******************************************************************************
+*   (c) 2018, 2019 ZondaX GmbH
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+********************************************************************************/
+// Integration tests
+
+#![deny(warnings, trivial_casts, trivial_numeric_casts)]
+#![deny(unused_import_braces, unused_qualifications)]
+#![deny(missing_docs)]
+
+#[macro_use]
+extern crate lazy_static;
+extern crate ledger_filecoin;
+#[macro_use]
+extern crate matches;
+extern crate secp256k1;
+extern crate sha2;
+
+use std::sync::Mutex;
+
+use ledger_filecoin::utils::{from_hex_string, to_hex_string};
+use ledger_filecoin::{BIP44Path, Error, FilecoinApp};
+use sha2::{Digest, Sha256};
+
+lazy_static! {
+    static ref APP: Mutex<FilecoinApp> = Mutex::new(FilecoinApp::connect().unwrap());
+}
+
+#[test]
+fn version() {
+    let app = APP.lock().unwrap();
+
+    let resp = app.version();
+
+    match resp {
+        Ok(version) => {
+            println!("mode  {}", version.mode);
+            println!("major {}", version.major);
+            println!("minor {}", version.minor);
+            println!("patch {}", version.patch);
+
+            assert_eq!(version.major, 0x00);
+            assert!(version.minor >= 0x04);
+        }
+        Err(err) => {
+            eprintln!("Error: {:?}", err);
+        }
+    }
+}
+
+#[test]
+fn address() {
+    let app = APP.lock().unwrap();
+    let path = BIP44Path {
+        purpose: 44,
+        coin: 461,
+        account: 0,
+        change: 0,
+        index: 5,
+    };
+    let resp = app.address(&path, false);
+
+    match resp {
+        Ok(addr) => {
+            assert_eq!(
+                to_hex_string(&addr.public_key.serialize()),
+                "0320316dba4ab1c0eb296467d69c32c6395af0cbc304e46f33e6929e9e6870bc3b"
+            );
+            assert_eq!(addr.formatted, "f1l332bk436joki7qojyckwmi32xa2zdpesvuitoi");
+
+            println!(
+                "Public Key  {:?}",
+                to_hex_string(&addr.public_key.serialize())
+            );
+            println!("Address     {:?}", addr.formatted);
+        }
+        Err(err) => {
+            eprintln!("Error: {:?}", err);
+            assert!(false);
+        }
+    }
+}
+
+#[test]
+fn address_testnet() {
+    let app = APP.lock().unwrap();
+    let path = BIP44Path {
+        purpose: 44,
+        coin: 1,
+        account: 0,
+        change: 0,
+        index: 5,
+    };
+    let resp = app.address(&path, false);
+
+    match resp {
+        Ok(addr) => {
+            assert_eq!(
+                to_hex_string(&addr.public_key.serialize()),
+                "033fc5ccea9872313b75fec78704b27420de29dd3db298a562559f90f332059465"
+            );
+            assert_eq!(addr.formatted, "t1bwr5ubfzbswspimbx2gauzmam2solgemalycbmi");
+
+            println!("Public Key {}", to_hex_string(&addr.public_key.serialize()));
+            println!("Address    {:?}", addr.formatted);
+        }
+        Err(err) => {
+            eprintln!("Error: {:?}", err);
+            assert!(false);
+        }
+    }
+}
+
+#[test]
+fn sign_empty() {
+    let app = APP.lock().unwrap();
+
+    let path = BIP44Path {
+        purpose: 44,
+        coin: 1,
+        account: 0,
+        change: 0,
+        index: 0,
+    };
+    let some_message0 = b"";
+    let signature = app.sign(&path, some_message0);
+    assert!(signature.is_err());
+    assert!(matches!(
+        signature.err().unwrap(),
+        Error::InvalidEmptyMessage
+    ));
+}
+
+#[test]
+fn sign_verify() {
+    let app = APP.lock().unwrap();
+
+    let txstr = "885501fd1d0f4dfcd7e99afcb99a8326b7dc459d32c6285501b882619d46558f3d9e316d11b48dcf211327025a0144000186a0430009c4430061a80040";
+    let blob = from_hex_string(txstr).unwrap();
+
+    let path = BIP44Path {
+        purpose: 44,
+        coin: 461,
+        account: 0,
+        change: 0,
+        index: 0,
+    };
+    match app.sign(&path, &blob) {
+        Ok(sig) => {
+            println!("{:#?}", to_hex_string(&sig.serialize_compact()));
+
+            // First, get public key
+            let addr = app.address(&path, false).unwrap();
+
+            let mut hasher = Sha256::new();
+            hasher.input(&blob);
+
+            let message = secp256k1::Message::from_slice(&hasher.result()).expect("32 bytes");
+
+            // Verify signature
+            let secp = secp256k1::Secp256k1::new();
+            assert!(secp.verify(&message, &sig, &addr.public_key).is_ok());
+        }
+        Err(e) => {
+            println!("Err {:#?}", e);
+            assert!(false);
+        }
+    }
+}
